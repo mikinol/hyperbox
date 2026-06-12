c284f0ea59bf0296176d09242ca748db3f3da3b4
mikinol <sm44aksdmiki13877kfh@gmail.com>
2026-06-12 16:21:49 +0300

    Сделал nix пакет

---
+++ default.nix
@@ -0,0 +1,21 @@
+{ stdenv, clang }:
+stdenv.mkDerivation {
+  pname = "hyperbox";
+  version = "1.0";
+
+  src = ./.;
+
+  buildPhase = ''
+    ${clang}/bin/clang -O3 -s -Wall -static -nostdlib -ffreestanding -fno-math-errno -fno-trapping-math -freciprocal-math -march=native -fassociative-math -fomit-frame-pointer main.c -o hyperbox
+  '';
+
+  installPhase = ''
+    mkdir -p $out/bin
+    cp hyperbox $out/bin/hyperbox
+    
+    ln -s hyperbox $out/bin/cat
+    ln -s hyperbox $out/bin/echo
+    ln -s hyperbox $out/bin/mkdir
+    ln -s hyperbox $out/bin/wc
+  '';
+}
