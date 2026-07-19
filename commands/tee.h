901cdb9690e61ea9933d76862765b795ccb5439a
mikinol <sm44aksdmiki13877kfh@gmail.com>
2026-07-19 19:41:15 +0300

    Упс, забыл чуть-чуть флаги (из-за этого права файлы были рандомными)

--- commands/tee.h
+++ commands/tee.h
@@ -4,7 +4,7 @@ noreturn static inline void do_tee(int argc, char **argv) {
   int fds[argc];
   fds[0] = STDOUT_FILENO;
   for (int i = 1; i < argc; i++) {
-    fds[i] = open(argv[i], O_WRONLY | O_CREAT | O_TRUNC);
+    fds[i] = open(argv[i], O_WRONLY | O_CREAT | O_TRUNC, 0666);
     if (fds[i] < 0) {
       print(&STDERR_IO, "Cannot open file ", argv[i], ": ", _errno, _endl);
       exit(1);
